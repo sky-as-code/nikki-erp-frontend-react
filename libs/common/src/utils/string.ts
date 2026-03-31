ad47a633e70c431c427536f17c326d2cda49d5ff
TanThanh-CityBoi <tanthanhe@gmail.com>
2026-03-31 17:54:39 +0700

    feat(util): convert camel to sneak

--- libs/common/src/utils/string.ts
+++ libs/common/src/utils/string.ts
@@ -1,9 +1,18 @@
 export const camelToSnakeCase = (str: string): string => {
-	return str.replace(/[A-Z]/g, (match) => `_${match.toLowerCase()}`);
+	return str
+		// userID → user_ID
+		.replace(/([a-z0-9])([A-Z])/g, '$1_$2')
+		// USERName → USER_Name (fix edge case)
+		.replace(/([A-Z]+)([A-Z][a-z])/g, '$1_$2')
+		.toLowerCase();
 };
 
 export const camelToUpperSnakeCase = (str: string): string => {
-	return str.replace(/[A-Z]/g, (match) => `_${match}`).toUpperCase();
+	return camelToSnakeCase(str).toUpperCase();
+};
+
+export const snakeToCamelCase = (str: string): string => {
+	return str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
 };
 
 export const splitByFromEnd = (str: string, step: number = 3): string[] => {
